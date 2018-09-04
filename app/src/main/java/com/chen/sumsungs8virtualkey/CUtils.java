71cbb82d05171ff2bec45944c6fbc42db53c3064
CHEN <921326671@qq.com>
2018-09-04 18:33:21 +0800

    调整判定返回view逻辑

--- app/src/main/java/com/chen/sumsungs8virtualkey/CUtils.java
+++ app/src/main/java/com/chen/sumsungs8virtualkey/CUtils.java
@@ -1,9 +1,14 @@
 package com.chen.sumsungs8virtualkey;
 
+import android.app.ActivityManager;
 import android.content.ComponentName;
 import android.content.Context;
 import android.provider.Settings;
 import android.text.TextUtils;
+
+import com.chen.sumsungs8virtualkey.utils.LogUtils;
+
+import java.util.List;
 
 /**
  * Created by CHEN on 2018/1/9.
@@ -27,4 +32,32 @@ public class CUtils {
         }
         return false;
     }
+
+
+    public static boolean isServiceRunning(Context mContext, String className) {
+
+        boolean isRunning = false;
+        ActivityManager activityManager = (ActivityManager) mContext
+                .getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningServiceInfo> serviceList = activityManager
+                .getRunningServices(30);
+
+        if (!(serviceList.size() > 0)) {
+            return false;
+        }
+
+        for (int i = 0; i < serviceList.size(); i++) {
+
+            LogUtils.e(serviceList.get(i).service.getClassName());
+
+            if (serviceList.get(i).service.getClassName().equals(className)) {
+
+                isRunning = true;
+                break;
+            }
+        }
+        return isRunning;
+
+    }
+
 }
--- app/src/main/java/com/chen/sumsungs8virtualkey/MainActivity.java
+++ app/src/main/java/com/chen/sumsungs8virtualkey/MainActivity.java
@@ -1,24 +1,13 @@
 package com.chen.sumsungs8virtualkey;
 
-import android.annotation.SuppressLint;
-import android.app.ActivityManager;
 import android.content.ComponentName;
-import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
 import android.graphics.Color;
-import android.graphics.PixelFormat;
 import android.net.Uri;
 import android.os.Build;
-import android.os.Message;
+import android.os.Bundle;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.Gravity;
-import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
@@ -26,26 +15,14 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.List;
+import com.chen.sumsungs8virtualkey.utils.LogUtils;
 
 
-public class MainActivity extends AppCompatActivity implements View.OnTouchListener,
-        IHandleMessage, View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     public static final int REQUEST_OVERLAY = 100;
 
     private static final String TAG = "MainActivityTouch";
-
-    private static final String HALF_BLACK = "#55000000";
-    private static final String TRANSLATE = "#00000000";
-
-    //定义浮动窗口布局
-    private RelativeLayout mFloatLayout;
-    private WindowManager.LayoutParams wmParams;
-    //创建浮动窗口设置布局参数的对象
-    private WindowManager mWindowManager;
-
-    private TextView mFloatView;
 
     private TextView mAccessStatus;
     private Button mAccessBtn;
@@ -74,11 +51,6 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
         setContentView(R.layout.activity_main);
         initPermission();
         initView();
-
-        mHander = new WeakRefHandler<>(this);
-
-        isServiceWork(getApplicationContext(), "com.chen.sumsungs8virtualkey.VirtualKeyService");
-
     }
 
     private void initView() {
@@ -140,33 +112,35 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
                 break;
 
             case R.id.color_bg_create:
-                createFloatView();
+                if (VirtualKeyService.isRunning())
+                    VirtualKeyService.getService().createFloatView();
+                else
+                    Toast.makeText(getApplicationContext(), R.string.please_try_again, Toast.LENGTH_SHORT).show();
                 checkAllpermiss();
                 break;
             case R.id.color_bg_destory:
-                destoryFlowView();
+                if (VirtualKeyService.isRunning())
+                    VirtualKeyService.getService().destoryFlowView();
+                else
+                    Toast.makeText(getApplicationContext(), R.string.please_try_again, Toast.LENGTH_SHORT).show();
                 checkAllpermiss();
                 break;
 
 
             case R.id.color_bg_commit:
                 //设置灰色
-                if (mFloatView != null)
-                    mFloatView.setBackgroundColor(Color.parseColor(HALF_BLACK));
+                if (VirtualKeyService.isRunning())
+                    VirtualKeyService.getService().setBgGray();
                 else
-                    Toast.makeText(getApplicationContext(), getString(R.string.get_permiss_first)
-                            , Toast.LENGTH_SHORT)
-                            .show();
+                    Toast.makeText(getApplicationContext(), R.string.please_try_again, Toast.LENGTH_SHORT).show();
                 break;
 
             case R.id.color_transparent:
                 //设置透明
-                if (mFloatView != null)
-                    mFloatView.setBackgroundColor(Color.parseColor(TRANSLATE));
+                if (VirtualKeyService.isRunning())
+                    VirtualKeyService.getService().setBgTran();
                 else
-                    Toast.makeText(getApplicationContext(), getString(R.string.get_permiss_first)
-                            , Toast.LENGTH_SHORT)
-                            .show();
+                    Toast.makeText(getApplicationContext(), R.string.please_try_again, Toast.LENGTH_SHORT).show();
                 break;
 
 
@@ -196,10 +170,10 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
                 intent.setData(Uri.parse("package:" + getPackageName()));
                 startActivityForResult(intent, REQUEST_OVERLAY);
             } else {
-                createFloatView();
+//                createFloatView();
             }
         } else {
-            createFloatView();
+//            createFloatView();
         }
     }
 
@@ -211,7 +185,10 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
         if (requestCode == REQUEST_OVERLAY) {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                 if (Settings.canDrawOverlays(this)) {
-                    createFloatView();
+                    if (VirtualKeyService.isRunning())
+                        VirtualKeyService.getService().createFloatView();
+                    else
+                        Toast.makeText(getApplicationContext(), R.string.please_try_again, Toast.LENGTH_SHORT).show();
                 } else {
                     Toast.makeText(this, "ACTION_MANAGE_OVERLAY_PERMISSION权限已被拒绝", Toast
                             .LENGTH_SHORT).show();
@@ -224,53 +201,53 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
     @Override
     protected void onResume() {
         super.onResume();
-
         checkAllpermiss();
+
+        LogUtils.e("是否运行中：" + VirtualKeyService.isRunning());
+
 
     }
 
 
     public void checkAllpermiss() {
 
-        if (mFloatView == null) {
-            mBackViewStatus.setText(R.string.no);
-        } else {
-            mBackViewStatus.setText(R.string.yes);
-        }
+
         //辅助功能
         if (VirtualKeyService.isRunning()) {
             mAccessStatus.setText(R.string.yes);
+
+
+            if (VirtualKeyService.getService().getmFloatView() == null) {
+                mBackViewStatus.setText(R.string.no);
+            } else {
+                mBackViewStatus.setText(R.string.yes);
+            }
+
+            //通知权限
+            if (CUtils.isEnabled(getApplicationContext())) {
+                mNotificationStatus.setText(R.string.yes);
+            } else {
+                mNotificationStatus.setText(R.string.no);
+            }
+
+            //窗口显示权限
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (Settings.canDrawOverlays(getApplicationContext())) {
+                    mWindowsStatus.setText(R.string.yes);
+                } else {
+                    mWindowsStatus.setText(R.string.no);
+                }
+            } else {
+                mWindowsStatus.setText(R.string.yes);
+            }
+
         } else {
             mAccessStatus.setText(R.string.no);
-        }
-
-        //通知权限
-        if (CUtils.isEnabled(getApplicationContext())) {
-            mNotificationStatus.setText(R.string.yes);
-        } else {
-            mNotificationStatus.setText(R.string.no);
-        }
-
-        //窗口显示权限
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && Settings.canDrawOverlays
-                (getApplicationContext())) {
-            mWindowsStatus.setText(R.string.yes);
-            createFloatView();
-        } else {
-            mWindowsStatus.setText(R.string.no);
-        }
-
-    }
-
-
-    private void destoryFlowView() {
-        if (mFloatLayout != null) {
-            //移除悬浮窗口
-            mWindowManager.removeView(mFloatLayout);
-            mFloatLayout = null;
+            if (VirtualKeyService.getService() != null)
+                if (VirtualKeyService.getService().getmFloatView() != null)
+                    VirtualKeyService.getService().destoryFlowView();
         }
     }
-
 
     /**
      * 打开辅助服务的设置
@@ -279,173 +256,9 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
         try {
             Intent intent = new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
             startActivity(intent);
-            Toast.makeText(this, "开启", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "请开启s8手势返回服务", Toast.LENGTH_LONG).show();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-
-    boolean isClick;
-    private boolean mIsMove;
-    private boolean mTouchTimeOut = false;
-
-
-    private long mDownTime;
-    private long mMoveTime;
-
-    private int mDownX;
-    private int mMoveX;
-
-
-    private WeakRefHandler mHander;
-
-    @Override
-    public boolean onTouch(View v, MotionEvent event) {
-        switch (event.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-
-                Log.e(TAG, "==========START================");
-                mIsMove = true;
-//                        mFloatView.setBackgroundResource(R.drawable.circle_red);
-                mDownTime = System.currentTimeMillis() / 1000;
-                mDownX = (int) event.getRawX()
-                        - mFloatView.getMeasuredWidth() / 2;
-                isClick = false;
-
-
-                if (!mTouchTimeOut)
-                    mHander.postDelayed(new Runnable() {
-                        @Override
-                        public void run() {
-                            if (mIsMove)
-                                mTouchTimeOut = true;
-                        }
-                    }, 500);
-
-                break;
-            case MotionEvent.ACTION_MOVE:
-                // getRawX是触摸位置相对于屏幕的坐标，getX是相对于按钮的坐标
-                wmParams.x = (int) event.getRawX()
-                        - mFloatView.getMeasuredWidth() / 2;
-                // 减25为状态栏的高度
-                wmParams.y = (int) event.getRawY()
-                        - mFloatView.getMeasuredHeight() / 2 - 75;
-
-
-                mMoveX = wmParams.x;
-                // 刷新
-//                Log.e("touch:", "X:" + wmParams.x + "/Y:" + wmParams.y);
-
-                boolean isMoveEnough = (mMoveX - mDownX) > 40;
-
-                Log.e("isMoveEnough:", "DownX:" + mDownX + "moveX:" + mMoveX);
-
-                if (mIsMove && isMoveEnough && !mTouchTimeOut) {
-                    isClick = true;
-                    mIsMove = false;
-                    VirtualKeyService.getService().clickBackKey();
-                    return true;
-                } else {
-                    Log.e(TAG, "mIsMove" + mIsMove + "/isMoveEnough" + isMoveEnough +
-                            "/mTouchTimeOut" + mTouchTimeOut);
-                    return false;
-                }
-
-            case MotionEvent.ACTION_UP:
-                mMoveX = 0;
-                mDownX = 0;
-                mTouchTimeOut = false;
-                Log.e(TAG, "==========STOP================");
-
-                mIsMove = false;
-//                        mFloatView.setBackgroundResource(R.drawable.circle_cyan);
-                return isClick;// 此处返回false则属于移动事件，返回true则释放事件，可以出发点击否。
-
-            default:
-                break;
-        }
-        return false;
-    }
-
-    @Override
-    public void onHandleMessage(Message msg) {
-        mTouchTimeOut = true;
-        Log.e(TAG, "============超时================");
-    }
-
-    @SuppressLint("ClickableViewAccessibility")
-    private void createFloatView() {
-
-        if (!VirtualKeyService.isRunning())
-            return;
-
-        if (mFloatLayout == null) {
-
-            wmParams = new WindowManager.LayoutParams();
-            //通过getApplication获取的是WindowManagerImpl.CompatModeWrapper
-            mWindowManager = (WindowManager) getApplication().getSystemService(WINDOW_SERVICE);
-            //设置window type
-            wmParams.type = WindowManager.LayoutParams.TYPE_PHONE;
-            //设置图片格式，效果为背景透明
-            wmParams.format = PixelFormat.RGBA_8888;
-            //设置浮动窗口不可聚焦（实现操作除浮动窗口外的其他可见窗口的操作）
-            wmParams.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE;
-            //调整悬浮窗显示的停靠位置为左侧置顶
-            wmParams.gravity = Gravity.LEFT | Gravity.TOP;
-//        wmParams.gravity = Gravity.BOTTOM;
-            // 以屏幕左上角为原点，设置x、y初始值，相对于gravity
-            wmParams.x = 0;
-            wmParams.y = 0;
-
-            //设置悬浮窗口长宽数据
-            wmParams.width = WindowManager.LayoutParams.WRAP_CONTENT;
-            wmParams.height = WindowManager.LayoutParams.MATCH_PARENT;
-
-            LayoutInflater inflater = LayoutInflater.from(this);
-            //获取浮动窗口视图所在布局
-            mFloatLayout = (RelativeLayout) inflater.inflate(R.layout.alert_window_menu, null);
-            //添加mFloatLayout
-            mWindowManager.addView(mFloatLayout, wmParams);
-            //浮动窗口按钮
-            mFloatView = mFloatLayout.findViewById(R.id.alert_window_imagebtn);
-
-            mFloatLayout.measure(View.MeasureSpec.makeMeasureSpec(0,
-                    View.MeasureSpec.UNSPECIFIED), View.MeasureSpec
-                    .makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
-
-
-            //设置监听浮动窗口的触摸移动
-            mFloatView.setOnTouchListener(this);
-
-            mFloatView.setOnClickListener(new View.OnClickListener() {
-
-                @Override
-                public void onClick(View v) {
-//                VirtualKeyService.getService().clickBackKey();
-                }
-            });
-        } else {
-            Toast.makeText(getApplicationContext(), "已经创建完毕", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-
-    public boolean isServiceWork(Context mContext, String serviceName) {
-        boolean isWork = false;
-        ActivityManager myAM = (ActivityManager) mContext
-                .getSystemService(Context.ACTIVITY_SERVICE);
-        List<ActivityManager.RunningServiceInfo> myList = myAM.getRunningServices(40);
-        if (myList.size() <= 0) {
-            return false;
-        }
-        for (int i = 0; i < myList.size(); i++) {
-            String mName = myList.get(i).service.getClassName().toString();
-            if (mName.equals(serviceName)) {
-                isWork = true;
-                break;
-            }
-        }
-        return isWork;
-    }
-
 }
